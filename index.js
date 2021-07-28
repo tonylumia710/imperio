c57f5920e56da8d1a356c372d4da6641c1db4a42
unknown <javiriki31@hotmail.com>
2021-07-28 16:15:35 -0300

    modificaciones wireframe

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html')); 
+})
+
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}\n http://localhost:${port}`))
