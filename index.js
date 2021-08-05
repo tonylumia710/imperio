73f02422d4dea45962e46ba0871019e1a9e4ef45
unknown <javiriki31@hotmail.com>
2021-08-05 15:13:09 -0300

    subiendo sprint2

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ let path = require('path');
 app.use(express.static('public'));
 
 /* Ruta para Home*/
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(path.join(__dirname,'views/index.html'));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/register.html'));
+    res.sendFile(path.join(__dirname,'views/register.html'));
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/login.html')); 
+    res.sendFile(path.join(__dirname,'views/login.html')); 
 })
 
-app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}\n http://localhost:${port}`))
+app.get('/password', (req, res) => {
+    res.sendFile(path.join(__dirname,'views/password.html')); 
+})
+
+app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}\n http://localhost:${port}`));
