d39bcda3e147a197867c558f6d04a2222a82836b
Antonio A. Alvarado <tonylumia710@hotmail.com>
2021-08-05 22:55:10 -0300

    carga de archivos de sprint 2

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ let path = require('path');
 app.use(express.static('public'));
 
 /* Ruta para Home*/
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(path.join(__dirname,'views/index.html'));
 })
 
-app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}\n http://localhost:${port}`))
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'views/login.html')); 
+})
+
+app.get('/password', (req, res) => {
+    res.sendFile(path.join(__dirname,'views/password.html')); 
+})
+
+app.get('/carrousel.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/carrousel.html'))
+})
+
+app.get('/carrito.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/carrito.html'))
+})
+
+app.get('/header.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/header.html'))
+})
+app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}\n http://localhost:${port}`));
