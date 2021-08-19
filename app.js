564fbddd6dcccf4c953584f266f7e1c68f24656f
unknown <javiriki31@hotmail.com>
2021-08-19 07:25:35 -0300

    22

--- app.js
+++ app.js
@@ -8,27 +8,27 @@ app.use(express.static('public'));
 /* Ruta para Home*/
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/index.html'));
+    res.sendFile(path.join(__dirname,'/src/views/index.html'));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/register.html'));
+    res.sendFile(path.join(__dirname,'/src/views/register.html'));
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/login.html')); 
+    res.sendFile(path.join(__dirname,'/src/views/login.html')); 
 })
 
 app.get('/password', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/password.html')); 
+    res.sendFile(path.join(__dirname,'/src/views/password.html')); 
 })
 
 app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/carrito.html'))
+    res.sendFile(path.join(__dirname, '/src/views/carrito.html'))
 })
 
 app.get('/productDetail', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+    res.sendFile(path.join(__dirname, '/src/views/productDetail.html'))
 })
 
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}\nhttp://localhost:${port}`));
